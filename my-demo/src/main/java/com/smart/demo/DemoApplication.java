4494a531da6136dd75ae51167057550bf06a7c36
weike <weikelujava@163.com>
2021-04-23 14:26:19 +0800

    初始化SpringBoot项目启动环境

---
+++ my-demo/src/main/java/com/smart/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.smart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lukewei
+ * @date 2021/4/23 14:24
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
